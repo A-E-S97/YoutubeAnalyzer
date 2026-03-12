14223c692103649608dd247907d08d9f0ca9d27d
A-E-S97 <emmanualakhil@gamin.com>
2026-03-12 17:51:01 +0530

    tools

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser, format_instructions
 from youtube_transcript_api import YouTubeTranscriptApi
+from tools import search_tool, save_tool
+load_dotenv()
 
 link = (input("Enter the youtube link: "))
 video_id = ""
@@ -15,5 +25,55 @@ try:
 except Exception as v:
     print("video_id is not accepted\n", v)
 
+
 print(len(transcript))
-print(transcript)
+
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    todo: list[str]
+    actions_items: list[str]
+nn = "gpt-4o-mini"
+llm = ChatOpenAI(model="gpt-5.4", api_key=os.getenv("OPENAI_API_KEY"))
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+            "system"
+            """
+            You will be provided with the transcript of a video and you should analyze it,
+            and generate a summary of the video/playlist and if the video is a guide
+            to do something, then you will create a todo list and action items.
+            Wrap the output in this format and provide no other text.
+            Always save the final response to a text file using the save_text_to_file tool.\n{format_instructions}
+            """
+            ),
+            ("placeholder", "{chat_history}"),
+            ("human", "{query}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+raw_response = agent_executor.invoke({"query": transcript})
+
+try:
+    structured_response = parser.parse(raw_response.get("output"))
+except Exception as e:
+    print("Error parsing response", e, "Raw Response: ", raw_response)
+
+print(raw_response)
+
+
+# import sys
+# print([k for k in sys.modules.keys() if 'pydantic.v1' in k])
---
+++ tools.py
@@ -0,0 +1,30 @@
+from idlelib.searchengine import search_reverse
+
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.tools import Tool
+from datetime import datetime
+
+
+def save_to_txt(data: str, filename_prefix: str = "video_analysis"):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    filename = f"{filename_prefix}_{timestamp}.txt"
+    formatted_text = f"---GPT Output---\nTimestamp: {timestamp}\n\n{data}\n\n"
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(formatted_text)
+    return f"Data successfully saved to {filename}"
+
+save_tool = Tool(
+    name = "save_text_to_file",
+    func = save_to_txt,
+    description = "Saves structured response to text file.",
+)
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name = "search",
+    func = search.run,
+    description = "Search DuckDuckGo for information to validate the information provided.",
+)
+
+# api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max= 300, wiki_client=search)
+# wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
